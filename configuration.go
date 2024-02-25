999017a78ced677f6d221ca71fe0cd3e8cc7e073
AzureDevOps Agent <bloodlab@dev.azure.com>
2024-02-25 21:40:42 +0000

    Version 1.3.1

--- configuration.go
+++ configuration.go
@@ -3,7 +3,7 @@ LogCom API
 
 LogCom Swagger documentation
 
-API version: 1.3.0
+API version: 1.3.1
 Contact: laborit@blutspende.de
 */
 
@@ -102,7 +102,7 @@ type Configuration struct {
 func NewConfiguration() *Configuration {
 	cfg := &Configuration{
 		DefaultHeader:    make(map[string]string),
-		UserAgent:        "OpenAPI-Generator/1.3.0/go",
+		UserAgent:        "OpenAPI-Generator/1.3.1/go",
 		Debug:            false,
 		Servers:          ServerConfigurations{
 			{
--- model_create_audit_log_change_dto.go
+++ model_create_audit_log_change_dto.go
@@ -3,7 +3,7 @@ LogCom API
 
 LogCom Swagger documentation
 
-API version: 1.3.0
+API version: 1.3.1
 Contact: laborit@blutspende.de
 */
 
