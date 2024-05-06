9b4d11482bb10df48dcd83e4ca6685bdc71a313c
AzureDevOps Agent <bloodlab@dev.azure.com>
2024-05-06 13:21:24 +0000

    Version 1.3.8

--- client.go
+++ client.go
@@ -3,7 +3,7 @@ LogCom API
 
 LogCom Swagger documentation
 
-API version: 1.3.6
+API version: 1.3.8
 Contact: laborit@blutspende.de
 */
 
@@ -42,7 +42,7 @@ var (
 	xmlCheck  = regexp.MustCompile(`(?i:(?:application|text)/xml)`)
 )
 
-// APIClient manages communication with the LogCom API API v1.3.6
+// APIClient manages communication with the LogCom API API v1.3.8
 // In most cases there should be only one, shared, APIClient.
 type APIClient struct {
 	cfg    *Configuration
